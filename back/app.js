a3c44bf9402ab857497b3a128d26f61de1f47f99
Peanuts-83 <tranque@free.fr>
2021-10-31 14:29:19 +0100

    better console.logs!

--- back/app.js
+++ back/app.js
@@ -1,48 +1,62 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Product = require('./models/product');
+const { json } = require('body-parser');
+
 const app = express();
 
-
-// HEADERS
+// HEADERS -- CORS management
 app.use((req,res,next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, PATCH');
   next();
-})
-// BODY-PARSER
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-// GET ALL
-app.get('/api/products', (req,res,next) => {
-  Product.find()
-    .then(products => res.status(200).json({'products': products}))
-    .catch(error => res.status(400).json(error));
-});
-// GET ONE
-app.get('/api/products/:id', (req,res,next) => {
-  Product.findOne({_id: req.params.id})
-    .then(product => res.status(200).json({'product': product}))
-    .catch(error => res.status(400).json(error));
-});
-app.post('/api/products', (req,res,next) => {
-  //delete req.body._id;
-  const product = new Product({...req.body});
-  product.save()
-    .then(() => res.status(201).json({'product': product }))
-    .catch(error => res.status(400).json({error}));
-});
-app.put('/api/products/:id', (req,res,next) => {
-  Product.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
-    .then(() => res.status(200).json({message: 'Modified!'}))
-    .catch(error => res.status(400).json({error}));
-})
-app.delete('/api/products/:id', (req,res,next) => {
-  Product.deleteOne({_id: req.params.id})
-    .then(() => res.status(200).json({message: 'Deleted!'}))
-    .catch(error => res.status(400).json({error}));
 });
 
+// BodyParser from Express -- reads form -> json (req.body)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// GET all
+app.get('/api/products', (req,res,next) => {
+  console.log('GETall URLreq:', req.url);
+  console.log('############');
+  Product.find()
+    .then(products => res.status(200).json({'products': products}))
+    .catch(err => res.status(400).json({ err }));
+});
+// GET one
+app.get('/api/products/:id', (req,res,next) => {
+  console.log('GETone URLreq:', req.url);
+  console.log('############');
+  Product.findOne({_id: req.params.id})
+    .then(product => res.status(200).json({'product': product}))
+    .catch(err => res.status(400).json({ err }));
+});
+// POST
+app.post('/api/products', (req,res,next) => {
+  console.log('POST URLreq:', req.url,'\nPOST BODYreq:', req.body);
+  console.log('############');
+  const product = new Product({...req.body});
+  product.save()
+    .then(product => res.status(201).json({'product': product }))
+    .catch(err => res.status(400).json({ err }));
+});
+// PUT
+app.put('/api/products/:id', (req,res,next) => {
+  console.log('PUT URLreq:', req.url,'\nPUT BODYreq:', req.body);
+  console.log('############');
+  Product.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+    .then(() => res.status(200).json({message: 'Modified!'}))
+    .catch(err => res.status(400).json({ err }));
+});
+// DELETE
+app.delete('/api/products/:id', (req,res,next) => {
+  console.log('DELETE URLreq:', req.url);
+  console.log('############');
+  Product.deleteOne({_id: req.params.id})
+    .then(() => res.status(200).json({message: 'Deleted!'}))
+    .catch(er=> res.status(400).json({ err }));
+});
 
 module.exports = app;
--- back/server.js
+++ back/server.js
@@ -2,19 +2,18 @@ const http = require('http');
 const mongoose = require('mongoose');
 const app = require('./app');
 
-// connect mongoDB
+// connect MongoDB
 mongoose.connect('mongodb+srv://Peanuts-83:H9pcNdqR4VT4jhqi@peanutsmongo.17rt9.mongodb.net/fullstack-activity?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
     .then(() => console.log('MongoDB connected!'))
-    .catch(error => console.log('MongoDB NOT connected -', error));
+    .catch(error => console.log('MongoDB NOT connected: ', error));
 
-
-// Normalize port
+// normalizePort
 const normalizePort = val => {
-  const port = parseInt(val, 10);
+  const port = parseint(val, 10);
   if (isNaN(port)) {
     return val;
   } else if (port >= 0) {
@@ -26,34 +25,34 @@ const normalizePort = val => {
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
-// ErrorHandler
+// errorHandler
 const errorHandler = error => {
   if (error.syscall != 'listen') {
     throw error;
   }
-  const address = server.adress();
-  const bind = typeof address === 'string' ?
-    'pipe ' + address : 'port: ' + address;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + 'requires elevated privileges.');
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + 'is already in use.');
-      break;
-    default:
-      throw error;
-  }
- }
+
+    switch(error.code) {
+      case 'EACCES':
+        console.log(bind, 'require elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.log(bind, 'is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+}
 
 const server = http.createServer(app);
+const address = server.address();
+const bind = typeof address === String ? 'pipe: ' + address : 'port: ' + port;
+console.log('- Server ONLINE -');
+
 server.on('error', errorHandler);
 server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ?
-  'pipe ' + address : 'port: ' + port;
-  console.log('Complete address:', address);
   console.log('Listening on', bind);
-})
+});
 
-server.listen(port);
+server.listen(process.env.PORT || 3000);
